20e6fe7c3a4e78e195a5bcaf81740b4ab7f4a27d
Marques Avery <averymarques4@gmail.com>
2020-09-10 00:39:46 +0000

    Automatically backed up by Learn

---
+++ lib/game.rb
@@ -0,0 +1,89 @@
+require 'pry'
+class Game
+  # include Players::Human
+
+  attr_accessor :board, :player_1, :player_2
+
+  WIN_COMBINATIONS = [
+     [0,1,2],
+     [3,4,5],
+     [6,7,8],
+     [0,3,6],
+     [1,4,7],
+     [2,5,8],
+     [0,4,8],
+     [2,4,6]
+     ]
+
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @board = board
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
+  def current_player
+    if @board.turn_count.even?
+      @player_1
+    elsif @board.turn_count.odd?
+      @player_2
+    end
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+
+      position_1 = @board.cells[win_index_1]
+      position_2 = @board.cells[win_index_2]
+      position_3 = @board.cells[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      end
+    end
+    false
+  end
+
+  def draw?
+    if @board.full?
+      if won? == false
+        true
+      end
+    end
+  end
+
+  def over?
+    won? || draw?
+  end
+
+  def winner
+    if won?
+      index = won?[0]
+      @board.cells[index]
+    end
+  end
+
+  def turn
+    index = current_player.move(@board)
+    while @board.valid_move?(index) == false
+      index = current_player.move(@board)
+    end
+    @board.update(index, current_player)
+  end
+
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    end
+    if draw?
+      puts "Cat's Game!"
+    end
+  end
+
+end
---
+++ lib/player.rb
@@ -0,0 +1,7 @@
+class Player
+  attr_reader :token, :name
+
+  def initialize(token)
+    @token = token
+  end
+end
