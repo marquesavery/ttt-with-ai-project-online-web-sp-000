14c61685a48e699256f884ba12e94459a92081d6
Marques Avery <averymarques4@gmail.com>
2020-09-10 01:03:28 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -112,7 +112,7 @@ class Game
     current_game.player_2.name = second_user
     play_again = " "
     until play_again == "N"
-      play
+      current_game.play
       puts "Would you like to play again? Enter Y/N."
       play_again = gets.chomp
     end
